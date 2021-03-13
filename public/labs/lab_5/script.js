324bf502e578eba7fd8216445014f4019c74a79f
Bapenni <44118863+Bapenni@users.noreply.github.com>
2021-03-13 01:39:35 -0500

    initial commit for HTML/map

--- public/labs/lab_5/script.js
+++ public/labs/lab_5/script.js
@@ -1,5 +1,14 @@
 function mapInit() {
   // follow the Leaflet Getting Started tutorial here
+  let map = L.map('mapid')
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiYmFwZW5uaSIsImEiOiJjazZtbGI1YXYwcjRhM2RwdWs3cnRhM3NhIn0.bk8wYJeohTxs3i-l-60gqg'
+  }).addTo(map);
   return map;
 }
 
